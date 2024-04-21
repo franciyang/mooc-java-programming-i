836702e9a872075fc0b8af7078a8b3f8618aef41
Francisco Yang <yangfranci@gmail.com>
2024-04-20 23:55:04 -0400

    Finished 7.2v2

---
+++ part07-Part07_02.LiquidContainers2/src/main/java/Container.java
@@ -0,0 +1,35 @@
+public class Container {
+    private int container;
+
+    public Container(){
+        this.container = 0;
+    }
+
+    public int contains(){
+        return this.container;
+    }
+
+    public void add(int amount){
+        if(amount > 0){
+            if(this.container + amount <= 100){
+                this.container += amount;
+            }else if(this.container + amount > 100){
+                this.container = 100;
+            }
+        }
+    }
+
+    public void remove(int amount){
+        if(amount > 0){
+            if(this.container - amount >= 0){
+                this.container -= amount;
+            }else if(this.container - amount < 0){
+                this.container = 0;
+            }
+        }
+    }
+
+    public String toString(){
+        return this.container + "/100";
+    }
+}
--- part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -5,16 +5,33 @@ public class LiquidContainers2 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        Container first = new Container();
+        Container second = new Container();
 
-
-        while (true) {
-            System.out.print("> ");
+        while (true){
+            System.out.println("First:" + first);
+            System.out.println("Second: " + second);
 
             String input = scan.nextLine();
             if (input.equals("quit")) {
                 break;
             }
-
+            String[] arrOfStr = input.split(" ");
+            String command = arrOfStr[0];
+            int value = Integer.valueOf(arrOfStr[1]);
+            if(command.equals("add")){
+                first.add(value);
+            }else if(command.equals("move")){
+                if(value > first.contains()){
+                    second.add(first.contains());
+                    first.remove(value);
+                }else{
+                    first.remove(value);
+                    second.add(value);
+                }
+            }else if(command.equals("remove")){
+                second.remove(value);
+            }
         }
     }
 
