ea487a175904456cd3d96c5a12a608b13c9da7b4
Francisco Yang <yangfranci@gmail.com>
2024-04-23 00:21:58 -0400

    7.3v2 Finished

--- part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -6,4 +6,59 @@ public class MainProgram {
         int[] array = {3, 1, 5, 99, 3, 12};
     }
 
+    public static int smallest(int[] array){
+        int smallest = array[0];
+        for(int i : array){
+            if(i < smallest){
+                smallest = i;
+            }
+        }
+        return smallest;
+    }
+
+    public static int indexOfSmallest(int[] array){
+        int smallest = array[0];
+        int index = 0;
+        for(int i = 0 ; i < array.length ; i++){
+            if(array[i] < smallest){
+                smallest = array[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    public static int indexOfSmallestFrom(int[] table, int startIndex){
+        int[] copy = table;
+        int index = startIndex;
+        int track = index;
+        int smallest = copy[index];
+        while(index < copy.length){
+            if(copy[index] < smallest){
+                smallest = copy[index];
+                track = index;
+                index++;
+            }else{
+                index++;
+            }
+        }
+        return track;
+    }
+
+    public static void swap(int[] array, int index1, int index2){
+        int first = index1;
+        int second = index2;
+        int hold = array[first];
+        array[first] = array[second];
+        array[second] = hold;
+    }
+
+    public static void sort(int[] array){
+        int[] copy = array;
+        for(int i = 0 ; i < array.length ; i++){
+            System.out.println(array);
+            int index = MainProgram.indexOfSmallestFrom(copy, i);
+            MainProgram.swap(copy,i,index);
+        }
+    }
 }
